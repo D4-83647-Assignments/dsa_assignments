e6cfb5b9a9cf5e0c5728d7245e3a379ca45eda0e
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-06-19 17:15:53 +0530

    Question1

---
+++ Assignment4/src/Stack.java
@@ -0,0 +1,29 @@
+
+public final class Stack extends SinglyLinearList{
+	
+	//stack will have Last in First Out
+		//1.add Last
+		//2.First out
+	
+	public Stack() {
+		
+	}
+	
+	
+	@Override
+	public void addLast(int value) {
+		super.addLast(value);
+	}
+	
+	@Override
+	public void deleteLast() {
+		super.deleteLast();
+	}
+	
+	@Override
+	public void displayList() {
+		super.displayList();
+		
+	}
+		
+}
