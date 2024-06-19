5176bdd8a562eebe626310d32670b207a737d56b
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-06-19 17:16:09 +0530

    Question1.Queue

---
+++ Assignment4/src/Queue.java
@@ -0,0 +1,29 @@
+
+public final class Queue extends SinglyLinearList{
+	//queue will have First in First Out
+		//1.add Last 
+		//2.First out
+
+	
+	public Queue() {
+		
+	}
+	
+	
+	@Override
+	public void addLast(int value) {
+		super.addLast(value);
+	}
+	
+	@Override
+	public void deleteFirst() {
+		super.deleteFirst();
+	}
+	
+	@Override
+	public void displayList() {
+		super.displayList();
+		
+	}
+
+}
